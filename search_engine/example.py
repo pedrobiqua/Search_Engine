db1e89fa2a940ad77ad38803f724f38a75432543
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2024-09-25 13:34:11 -0300

    Testing import c++ function cython

--- search_engine/example.py
+++ search_engine/example.py
@@ -1,4 +1,13 @@
-from hello import PyHello
+from _page_rank import PyGraph
 
-def ola(hello: PyHello):
-    print(hello.greet("World"))
+def montargrafo_print():
+    graph = PyGraph()
+
+    graph.add_edge(1, 3)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 4)
+
+    graph.print()
+
+
+# montargrafo_print()
