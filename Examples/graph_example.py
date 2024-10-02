cf97270d6519bb9bfbedc9d9f579d5384ee024cd
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2024-10-02 13:08:20 -0300

    Update Example

--- Examples/graph_example.py
+++ Examples/graph_example.py
@@ -1,6 +1,18 @@
+# EXEMPLO USANDO A BIBLIOTECA SEARCH_ENGINE
+
 from search_engine.crawler import Crawler
 
-crawler = Crawler("https://pt.wikipedia.org", "/wiki/", "Gato")
+# Rodar com o test_mode ativado ajuda a testar a lib e fazer com que o parametro limit seja usado
 
+# O CRAWLER É RESPONSAVEL POR VARER AS PÁGINAS
+crawler = Crawler("https://pt.wikipedia.org", "/wiki/", "Gato", test_mode=True)
 
-crawler.run()
+# QUANDO TERMINA DE RODAR ISSO SERÁ GERADO UM GRAFO
+grafo = crawler.run(limit=10)
+# grafo.print()
+
+# COM O GRAFO CONSEGUIMOS COMPUTAR O PAGE_RANK DAS PÁGINAS
+meu_dict = grafo.compute_page_rank()
+top_tres = sorted(meu_dict.items(), key=lambda item: item[1], reverse=True)[:3]
+
+print(top_tres)
