598adb5156d064437cce9e049dd1dbc902fbba12
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2024-10-11 11:43:49 -0300

    Create tests for inverted_index (ref #1)

---
+++ tests/unit-tests/inverted_index-test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include "inverted_index.h"
+
+// Teste para verificar a adição de texto
+TEST(InvertedIndexTest, AddEdge) {
+    
+    
+
+    // TODO: Lembrar de remover isso
+    int actual = 1;
+    int expected = 1;
+    EXPECT_EQ(actual, expected);
+}
