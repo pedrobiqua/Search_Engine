3d650e717e623deb177da945998faf5b68fcaf7e
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2024-12-17 16:15:55 -0300

    [Docs #14] Add docs using doxygen

--- search_engine/exceptions.py
+++ search_engine/exceptions.py
@@ -1,5 +1,29 @@
 class UrlError(ValueError):
-    """Raised when an invalid URL is provided."""
+    """
+    Exception raised when an invalid URL is provided.
+
+    Inherits from the built-in `ValueError` class.
+
+    Attributes
+    ----------
+    message : str
+        The error message that describes the cause of the exception.
+        Defaults to "Invalid URL".
+
+    Methods
+    -------
+    __init__(self, message: str = "Invalid URL")
+        Initializes the exception with a custom message or a default message.
+    """
+
     def __init__(self, message="Invalid URL"):
+        """
+        Initializes the UrlError exception with a custom error message.
+
+        Parameters
+        ----------
+        message : str, optional
+            The error message to describe the exception. Default is "Invalid URL".
+        """
         self.message = message
         super().__init__(self.message)
--- search_engine/helper/converter.py
+++ search_engine/helper/converter.py
@@ -1,5 +1,37 @@
 class StringToIntConverter:
+    """
+    A class that converts strings to integers. If a string has already been converted,
+    it returns the previously assigned integer. Otherwise, it assigns a new integer
+    to the string and returns it.
+
+    Attributes
+    ----------
+    string_to_int : dict
+        A dictionary mapping strings to their corresponding integers.
+    next_int : int
+        The next integer to be assigned to a string.
+
+    Methods
+    -------
+    __init__()
+        Initializes the converter with an empty dictionary and sets the next integer to 0.
+    convert(word: str) -> int
+        Converts a given string to an integer. If the string has been seen before,
+        it returns the corresponding integer, otherwise it assigns a new integer to the string.
+    """
+
     def __init__(self):
+        """
+        Initializes the StringToIntConverter with an empty dictionary and sets the next integer
+        to 0 for string-to-integer mapping.
+
+        Attributes
+        ----------
+        string_to_int : dict
+            An empty dictionary to store the mapping of strings to integers.
+        next_int : int
+            Initialized to 0, it represents the next available integer to be assigned to a string.
+        """
         self.string_to_int = {}
         self.next_int = 0
 
@@ -7,12 +39,12 @@ class StringToIntConverter:
         """
         Convert a string to an integer. If the string has already been seen,
         return its corresponding integer. Otherwise, assign a new integer to it.
-        
+
         Parameters
         ----------
         word : str
             The string to be converted.
-        
+
         Returns
         -------
         int
