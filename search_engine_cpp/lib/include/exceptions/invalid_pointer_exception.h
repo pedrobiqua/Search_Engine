e255e6ac4f6093c890ba76fb499380d97afe1bf0
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2025-01-31 14:35:44 -0300

    Add coments

--- search_engine_cpp/lib/include/exceptions/invalid_pointer_exception.h
+++ search_engine_cpp/lib/include/exceptions/invalid_pointer_exception.h
@@ -5,12 +5,31 @@
 #include <string>
 
 namespace exceptions {
+
+/**
+ * @class invalid_pointer_exception
+ * @brief Exception indicating the use of an invalid pointer.
+ *
+ * This exception is derived from `std::exception` and can be thrown
+ * when an invalid pointer is detected.
+ */
 class invalid_pointer_exception : public std::exception {
    public:
+    /**
+     * @brief Constructor for the invalid_pointer_exception class.
+     *
+     * Initializes a new instance of the exception.
+     */
     invalid_pointer_exception();
 
+    /**
+     * @brief Returns a descriptive error message.
+     *
+     * @return A C-style string describing the error.
+     */
     const char* what() const noexcept override;
 };
+
 }  // namespace exceptions
 
-#endif
+#endif  // INVALID_POINTER_H
